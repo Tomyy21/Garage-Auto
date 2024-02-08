37dbbcc0f618b8471e7761c8ad03b7b7261c459e
Tomy <tomyouadhi21@gmail.com>
2024-02-08 23:53:18 +0100

    ajout de la version longue des services dur les deux premiers

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ menu_toggle.onclick = function () {
   menu_toggle_span.classList.toggle("active");
   menu.classList.toggle("responsive");
 };
+
+// -------------------------------
+
+let LirePlus1 = document.querySelector(".read_more1");
+let LirePlus2 = document.querySelector(".read_more2");
+let LirePlus3 = document.querySelector(".read_more3");
+const service = document.querySelector(".service");
+const depannage = document.querySelector(".depannage");
+const textdepannage = document.querySelector(".textdepannage");
+const textdepannagelong = document.querySelector(".textdepannagelong");
+
+const service2 = document.querySelector(".service2");
+const carroserie = document.querySelector(".carroserie");
+const textcarroserie = document.querySelector(".textcarroserie");
+const textdecarroserielong = document.querySelector(".textdecarroserielong");
+
+LirePlus1.addEventListener("click", () => {
+  service.classList.toggle("serviceAvantage");
+  depannage.classList.toggle("depannageAvantage");
+  textdepannage.classList.toggle("textdepannageAvantage");
+  textdepannagelong.classList.toggle("textdepannagelongAvantage");
+});
+
+LirePlus2.addEventListener("click", () => {
+  service2.classList.toggle("serviceAvantage2");
+  carroserie.classList.toggle("carroserieAvantage");
+  textcarroserie.classList.toggle("textcarroserieAvantage");
+  textdecarroserielong.classList.toggle("textdecarroserielongAvantage");
+});
+
+LirePlus3.addEventListener("click", () => {
+  console.log("t'a clické 3");
+});
